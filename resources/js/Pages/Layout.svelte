c8ce042aa02c06821504c08e213bad0f56606297
Visnicio <vinicius.antonio0712@gmail.com>
2023-05-14 20:31:21 -0300

    fix external links

--- resources/js/Pages/Layout.svelte
+++ resources/js/Pages/Layout.svelte
@@ -7,7 +7,7 @@
   <div class="py-3 container mx-auto flex justify-between items-center">
     <div use:inertia href="/" class="font-bold text-2xl bg-gradient-to-r from-purple-700 to-orange-500 bg-clip-text text-transparent">Laravelte</div>
     <div>
-      <a href="https://github.com/Visnicio/laravel-svelte-tailwind" class="mr-2">Github</a>
+      <a href="https://github.com/Visnicio/laravel-svelte-daisyui" class="mr-2">Github</a>
     </div>
   </div>
 </header>
